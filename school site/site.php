38cf3ba14a608726e71e72edd100ee7774dbc6ad
PrinceJoshua20 <138766271+PrinceJoshua20@users.noreply.github.com>
2024-02-16 13:49:37 +0000

    Add files via upload

---
+++ school site/site.php
@@ -0,0 +1,83 @@
+<?php
+// Database connection
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "db_school"; 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Sign Up
+if (isset($_POST['signup'])) {
+    $name = $_POST['name'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password for security
+
+    $sql = "INSERT INTO teachers (name, username, email, password) VALUES ('$name', '$username', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Sign up successful";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+
+// Sign In
+if (isset($_POST['signin'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM teachers WHERE username='$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            echo "<script>window.location.href='tdashboard.php'</script>";
+            // Redirect to dashboard or any other page
+
+        } else {
+            echo "Invalid username or password";
+        }
+    } else {
+        echo "Invalid username or password";
+    }
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="site.css">
+
+<head>
+    <title>Teacher Signup and Signin</title>
+</head>
+<body>
+
+    <h2>Sign In</h2>
+    <form method="post">
+        <input type="text" name="username" placeholder="Username" required><br><br>
+        <input type="password" name="password" placeholder="Password" required><br><br>
+        <input type="submit" name="signin" value="Sign In">
+    </form>
+    <h2>Sign Up</h2>
+    <form method="post">
+        <input type="text" name="name" placeholder="Name" required><br><br>
+        <input type="text" name="username" placeholder="Username" required><br><br>
+        <input type="email" name="email" placeholder="Email" required><br><br>
+        <input type="password" name="password" placeholder="Password" required><br><br>
+        <input type="submit" name="signup" value="Sign Up">
+    </form>
+
+</body>
+</html>
---
+++ school site/teachers.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 16, 2024 at 02:42 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`name`, `username`, `email`, `password`) VALUES
+('joe', 'lol', '4231220160@live.gctu.edu.gh', '0'),
+('josh', 'Good site', '4231220160@live.gctu.edu.gh', '$2y$10$tqzuDe0ZMFXlPgkXwExYou8rNot0p3VFy/rme8RRn2jl0ZdxaXMAe');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
