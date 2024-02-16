58571910a8caf80ce0b1a00a8600b2d778358934
RaphaelWright <raphaelwright2003@gmail.com>
2024-02-16 09:46:55 +0000

    test

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection
-$servername = "localhost"; // Change this to your database server
-$username = "root"; // Change this to your database username
-$password = " "; // Change this to your database password
-$dbname = "teacher_db"; // Change this to your database name
+$servername = "localhost"; 
+$username = "root"; 
+$password = " "; 
+$dbname = "teacher_db"; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
